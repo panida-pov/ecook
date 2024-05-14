45e3b398b80e85110ed22271f6a93c5292f13b94
panida-pov <panida.pov@gmail.com>
2024-05-14 20:01:27 +0900

    styling

---
+++ ecook-client/src/utils/styles.ts
@@ -0,0 +1,21 @@
+type Styles = {
+  bgColors: string[];
+  heartColors: string[];
+};
+
+export const styles: Styles = {
+  bgColors: [
+    "rgba(253, 238, 185, 1)", // yellow
+    "rgba(220, 234, 247, 1)", // blue
+    "rgba(251, 227, 214, 1)", // orange
+    "rgba(217, 242, 208, 1)", // green
+    "rgba(248, 228, 246, 1)", // pink
+  ],
+  heartColors: [
+    "rgba(234, 173, 0,1)", // dark yellow
+    "rgba(78, 149, 217, 1)", // dark blue
+    "rgba(192, 79, 21, 1)", // dark orange
+    "rgba(59, 125, 35, 1)", // dark green
+    "rgba(120, 32, 110,1)", // dark pink
+  ],
+};
