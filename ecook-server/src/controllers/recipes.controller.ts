e61772596f02f7afd279b70850a502a59928197a
panida-pov <panida.pov@gmail.com>
2024-05-28 22:04:24 +0900

    update recipe api (get/post/delete)

--- ecook-server/src/controllers/recipes.controller.ts
+++ ecook-server/src/controllers/recipes.controller.ts
@@ -4,75 +4,140 @@ import { UpdateFavoriteDto } from "../dto/recipes/UpdateFavorite.dto";
 import { RecipelistResponse } from "../dto/recipes/RecipelistResponse.dto";
 import { RecipeResponse } from "../dto/recipes/RecipeResponse.dto";
 import { CustomError } from "../utils/CustomError";
+import { myDataSource } from "../data-source";
+import { RecipeEntity } from "../entity/recipe.entity";
+import { MethodEntity } from "../entity/method.entity";
+import { IngredientEntity } from "../entity/ingredient.entity";
+import { RecipeLabelEntity } from "../entity/recipeLabel.entity";
+import { LabelEntity } from "../entity/label.entity";
 
 // GET: /api/recipes
-export function getRecipes(
+export async function getRecipes(
   req: Request,
   res: Response<Array<RecipelistResponse>>,
   next: NextFunction
 ) {
   try {
-    res.status(200).send([
-      {
-        id: 1,
-        name: "Teriyaki chicken",
-        favorite: false,
-        labels: ["asian"],
-      },
-      {
-        id: 2,
-        name: "Gapao rice",
-        favorite: true,
-        labels: ["thai", "asian"],
-      },
-      {
-        id: 3,
-        name: "Spaghetti Bolognese",
-        favorite: false,
-        labels: ["western"],
-      },
-    ]);
-    // const error = new CustomError("recipes not found", 404);
+    const recipes = await myDataSource.getRepository(RecipeEntity).find({
+      relations: { recipeLabels: { label: true } },
+    });
+
+    const results = recipes.map((recipe) => {
+      const result = new RecipelistResponse();
+      result.id = recipe.id;
+      result.name = recipe.name;
+      result.favorite = recipe.favorite;
+      result.labels = recipe.recipeLabels.map((label) => label.label.name);
+      return result;
+    });
+
+    res.status(200).send(results);
   } catch (e) {
     next(e);
   }
 }
 
 // POST: /api/recipes
-export function createRecipe(
+export async function createRecipe(
   req: Request<Record<string, never>, Record<string, never>, CreateRecipeDto>,
   res: Response<RecipeResponse>,
   next: NextFunction
 ) {
-  res.status(201).send();
+  try {
+    const { name, favorite, labels, servings, ingredients, methods } = req.body;
+
+    const allLabels = await myDataSource.getRepository(LabelEntity).find();
+    const labelIds: number[] = labels.map((label) => {
+      const found = allLabels.find((item) => item.name === label);
+
+      if (!found) {
+        throw new CustomError("Labels provided do not exist!", 400);
+      }
+
+      return found.id;
+    });
+
+    const newRecipe = await myDataSource
+      .getRepository(RecipeEntity)
+      .save({ name, favorite, servings });
+
+    await Promise.all(
+      labelIds.map((labelId) =>
+        myDataSource
+          .getRepository(RecipeLabelEntity)
+          .save({ recipeId: newRecipe.id, labelId: labelId })
+      )
+    );
+
+    const newIngredients = await Promise.all(
+      ingredients.map((ingredient) => {
+        return myDataSource.getRepository(IngredientEntity).save({
+          recipeId: newRecipe.id,
+          amount: ingredient.amount,
+          unit: ingredient.unit,
+          name: ingredient.name,
+        });
+      })
+    );
+
+    const newMethod = await myDataSource
+      .getRepository(MethodEntity)
+      .save({ method: methods.join("\n") });
+
+    const result = new RecipeResponse();
+    result.id = newRecipe.id;
+    result.name = newRecipe.name;
+    result.favorite = newRecipe.favorite;
+    result.labels = labels;
+    result.ingredients = newIngredients.map((ingredient) => {
+      return {
+        amount: ingredient.amount,
+        unit: ingredient.unit,
+        name: ingredient.name,
+      };
+    });
+    result.methods = newMethod.method.split("\n");
+
+    res.status(201).send(result);
+  } catch (e) {
+    next(e);
+  }
 }
 
 // GET: /api/recipes/:id
-export function getRecipeById(
+export async function getRecipeById(
   req: Request<{ id: number }>,
   res: Response<RecipeResponse>,
   next: NextFunction
 ) {
   try {
     const { id } = req.params;
-    if (id === 1) {
-      return res.status(200).send({
-        id: 1,
-        name: "Teriyaki chicken",
-        favorite: true,
-        labels: ["asian"],
-        servings: 2,
-        ingredients: [
-          {
-            name: "soy sauce",
-            amount: 1 / 4,
-            unit: "cup",
-          },
-        ],
-        methods: ["first method", "second method"],
-      });
+    const recipe = await myDataSource.getRepository(RecipeEntity).findOne({
+      relations: { ingredients: true, method: true },
+      where: { id: id },
+    });
+
+    if (!recipe) {
+      return next(
+        new CustomError(`Recipe with id = ${id} doesn't exist!`, 404)
+      );
     }
-    next(new CustomError(`Can't find recipe with ID=${id}`, 404));
+
+    const result = new RecipeResponse();
+    result.id = recipe.id;
+    result.name = recipe.name;
+    result.favorite = recipe.favorite;
+    result.servings = recipe.servings;
+    result.ingredients = recipe.ingredients.map((ingredient) => {
+      return {
+        amount: ingredient.amount,
+        unit: ingredient.unit,
+        name: ingredient.name,
+      };
+    });
+    result.methods = recipe.method.method.split("\n");
+
+    res.status(200).send(result);
   } catch (e) {
     next(e);
   }
@@ -81,20 +146,36 @@ export function getRecipeById(
 // PUT: /api/recipes/:id
 export function updateRecipe(
   req: Request<{ id: number }, Record<string, never>, CreateRecipeDto>,
-  res: Response<RecipeResponse>
+  res: Response<RecipeResponse>,
+  next: NextFunction
 ) {
   res.status(202).send();
 }
 
 // DELETE: /api/recipes/:id
-export function deleteRecipe(req: Request<{ id: number }>, res: Response) {
-  res.status(204).send();
+export async function deleteRecipe(
+  req: Request<{ id: number }>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { affected } = await myDataSource
+      .getRepository(RecipeEntity)
+      .delete(req.params.id);
+    if (!affected) {
+      return next(new CustomError("No recipe to delete!", 404));
+    }
+    return res.status(204).send();
+  } catch (e) {
+    next(e);
+  }
 }
 
 // PATCH: /api/recipes/:id
 export function updateFavorite(
   req: Request<{ id: number }, Record<string, never>, UpdateFavoriteDto>,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) {
   res.status(202).send();
 }
