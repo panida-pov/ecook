ac38e1453eb82065ec5df4a2a94ccfae0147ce2e
panida-pov <panida.pov@gmail.com>
2024-05-29 16:56:12 +0900

    create label api

--- ecook-server/src/controllers/labels.contoller.ts
+++ ecook-server/src/controllers/labels.contoller.ts
@@ -1,70 +1,103 @@
 import { Request, Response, NextFunction } from "express-serve-static-core";
-import { LabelResponse } from "../dto/labels/LabelResponse.dto";
 import { CreateLabelDto } from "../dto/labels/CreateLabel.dto";
+import { myDataSource } from "../data-source";
+import { LabelEntity } from "../entity/label.entity";
+import { CustomError } from "../utils/CustomError";
+import { LabelResponse } from "../dto/labels/LabelResponse.dto";
 
 // GET: /api/labels
-export function getLabels(
+export async function getLabels(
   req: Request,
   res: Response<Array<LabelResponse>>,
   next: NextFunction
 ) {
   try {
-    res.status(200).send([
-      { id: 1, name: "thai" },
-      { id: 2, name: "western" },
-      { id: 3, name: "dessert" },
-    ]);
+    const labels = await myDataSource
+      .getRepository(LabelEntity)
+      .find({ order: { id: "ASC" } });
+    res.status(200).send(labels);
   } catch (e) {
     next(e);
   }
 }
 
 // POST: /api/labels
-export function createLabel(
+export async function createLabel(
   req: Request<Record<string, never>, Record<string, never>, CreateLabelDto>,
   res: Response<LabelResponse>,
   next: NextFunction
 ) {
   try {
-    res.status(201).send({ id: 1, name: req.body.name });
+    const newLabel = await myDataSource
+      .getRepository(LabelEntity)
+      .save({ name: req.body.name });
+    res.status(201).send({ id: newLabel.id, name: newLabel.name });
   } catch (e) {
     next(e);
   }
 }
 
 // GET: /api/labels/:id
-export function getLabelById(
+export async function getLabelById(
   req: Request<{ id: number }>,
   res: Response<LabelResponse>,
   next: NextFunction
 ) {
   try {
-    res.status(200).send({ id: req.params.id, name: "get" });
+    const { id } = req.params;
+    const label = await myDataSource
+      .getRepository(LabelEntity)
+      .findOneBy({ id: id });
+    if (!label) {
+      return next(new CustomError(`Label with id = ${id} doesn't exist!`, 404));
+    }
+    res.status(200).send(label);
   } catch (e) {
     next(e);
   }
 }
 
 // PUT: /api/labels/:id
-export function updateLabel(
+export async function updateLabel(
   req: Request<{ id: number }, Record<string, never>, CreateLabelDto>,
   res: Response<LabelResponse>,
   next: NextFunction
 ) {
   try {
-    res.status(202).send({ id: req.params.id, name: req.body.name });
+    const { id } = req.params;
+    const label = await myDataSource
+      .getRepository(LabelEntity)
+      .findOneBy({ id: id });
+    if (!label) {
+      return next(new CustomError(`Label with id = ${id} doesn't exist!`, 404));
+    }
+
+    if (label.name !== req.body.name) {
+      await myDataSource
+        .getRepository(LabelEntity)
+        .save({ ...label, name: req.body.name });
+    }
+
+    res.status(202).send();
   } catch (e) {
     next(e);
   }
 }
 
 // DELETE: /api/labels/:id
-export function deleteLabel(
+export async function deleteLabel(
   req: Request<{ id: number }>,
   res: Response,
   next: NextFunction
 ) {
   try {
+    const { affected } = await myDataSource
+      .getRepository(LabelEntity)
+      .delete(req.params.id);
+    if (!affected) {
+      return next(new CustomError("No label to delete!", 404));
+    }
+
     res.status(204).send();
   } catch (e) {
     next(e);
