7589859b4d1d01ab007fcf1e1fa55a03346852bd
panida-pov <panida.pov@gmail.com>
2024-06-25 19:17:46 +0900

    update env variable names

--- ecook-server/src/data-source.ts
+++ ecook-server/src/data-source.ts
@@ -2,8 +2,8 @@ import { DataSource } from "typeorm";
 
 export const myDataSource = new DataSource({
   type: "mysql",
-  host: process.env.HOST || "localhost",
-  port: parseInt(process.env.PORT || "23307"),
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "23307"),
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_DBNAME || "ecook",
