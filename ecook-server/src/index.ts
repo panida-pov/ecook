a70f2546ce86fdd2c1bb3f6f7dbc02f0e3970be2
panida-pov <panida.pov@gmail.com>
2024-06-25 19:51:03 +0900

    remove 'localhost' from console.log

--- ecook-server/src/index.ts
+++ ecook-server/src/index.ts
@@ -32,5 +32,5 @@ app.use(globalErrorHandler);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at port ${port}`);
 });
