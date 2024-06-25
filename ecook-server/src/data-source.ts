d18f109fe5e7905ff47b42a7305595224ec5ac93
panida-pov <panida.pov@gmail.com>
2024-06-25 19:07:29 +0900

    setup env variables in datasource

--- ecook-server/src/data-source.ts
+++ ecook-server/src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const myDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 23307,
-  username: "root",
-  password: "root",
-  database: "ecook",
+  host: process.env.HOST || "localhost",
+  port: parseInt(process.env.PORT || "23307"),
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_DBNAME || "ecook",
   entities: [__dirname + "/**/*.entity{.ts,.js}"],
   migrationsRun: false,
   migrations: [__dirname + "/migration/*{.ts,.js}"],
